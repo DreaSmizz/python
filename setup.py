c661df29c59ffa82fa70010f93819dda132bfba0
DreaSmizz <DreaSmizz@users.noreply.github.com>
2019-02-20 13:30:37 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from setuptools import setup
+from sys import path
+
+setup(
+		name         = 'nestor',
+		version      = '1.0.0',
+        #packages     = find_packages(),
+		py_modules   = ['nestor'],
+		author       = 'andrea smith',
+		author_email = 'a.smith102781@gmail.com'
+		#url          = 'https://github.com/DreaSmizz/python.git',
+		#description  = 'A simple printer of nested lists',
+		)
