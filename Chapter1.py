f2acf527ebb8c40e7f28b601e201b6b202217658
Andrea Smith <a.smith102781@gmail.com>
2019-02-21 12:44:37 -0500

    Updated Chapter 1 code

--- Chapter1.py
+++ Chapter1.py
@@ -1,2 +1,11 @@
 movies = ["THe Holy Grail", "The Life of Brian", "The Meaning of Life"]
 print(movies[1])
+cast = ["Cleese", 'Palin', 'Jones', "Idle"]
+print(len(cast))
+print(cast[1])
+cast.append("Gilliam")
+print(cast)
+cast.pop()
+print(cast)
+cast.extend(["Gilliam", "Chapman"])
+print(cast)
