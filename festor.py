1e871acc720f939c52605fc20412b87084257eff
Andrea Smith <a.smith102781@gmail.com>
2019-02-21 16:14:39 -0500

    Updated code

--- festor.py
+++ festor.py
@@ -12,4 +12,4 @@ def print_lol(the_list):
 movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91, ["Graham Chapman", ["Micheal Palin", "John Cleese",
 "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
 
-print_lol(movies)
+print_lol(movies) 
