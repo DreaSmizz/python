1c7ee921052a26b267ea99a7115a10f77cc17be7
Andrea Smith <a.smith102781@gmail.com>
2019-02-21 13:59:01 -0500

    updating chapter1 code

--- Chapter1.py
+++ Chapter1.py
@@ -9,3 +9,33 @@ cast.pop()
 print(cast)
 cast.extend(["Gilliam", "Chapman"])
 print(cast)
+movies.insert(1, 1975)
+movies.insert(3, 1979)
+movies.append(1983)
+print(movies)
+fav_movies = ["The Holy Grail", "The Life of Brian"]
+for each_flick in fav_movies:
+    print(each_flick)
+
+movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91, 
+            ["Graham Chapman", ["Micheal Palin", "John Cleese", 
+                "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+print(movies)
+for each_item in movies:
+    print(each_item)
+
+names = ['Micheal', 'Terry']
+isinstance(names, list)
+num_names = len(names)
+isinstance(num_names, list)
+
+for each_item in movies:
+    if isinstance(each_item, list):
+        for nested_item in each_item:
+            if isinstance(nested_item, list):
+                for deeper_item in nested_item:
+                    print(deeper_item)                
+            else:
+                print(nested_item)
+    else:
+        print(each_item)
