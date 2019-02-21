8cb98b1db3eaf5df1e0e9f3695916e9fa3763bba
Andrea Smith <a.smith102781@gmail.com>
2019-02-21 15:20:37 -0500

    updating files

--- Chapter1.py
+++ Chapter1.py
@@ -39,3 +39,16 @@ for each_item in movies:
                 print(nested_item)
     else:
         print(each_item)
+
+the_list = ["Chicken", "Soda", "Fries"]
+
+def print_lol(the_list):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
+        else:
+            print(each_item)
+
+            
+
+
