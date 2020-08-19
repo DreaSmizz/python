55c642775092574b440bb7495ca81d169b82483c
Andrea Smith <a.smith102781@gmail.com>
2020-08-19 12:24:07 -0400

    Commit changes

---
+++ FirstProgram/mileage.py
@@ -0,0 +1,11 @@
+print("How many kilometers did you cycle today?")
+kms = input()
+miles = float(kms)/1.60934
+miles = round(miles, 2)
+print(f"Your {kms}km is equal to {miles} miles")
+
+for number in range(1,20):
+    print(number)
+
+for letter in "coffee":
+    print(letter)
